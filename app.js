bfb6b39d0c3f1d91b21f9faa708f3f0dc62f0e28
pshenichque <pshenichque@mail.ru>
2023-07-22 01:08:54 +0300

    Added addition, subtraction, multiplication and division functions

--- app.js
+++ app.js
@@ -1,4 +1,47 @@
-const resultElement = document.getElementById('result')
-console.log(resultElement.textContent)
-resultElement.textContent = 42
+const inputFirst = document.getElementById('input1');
+const inputSecond = document.getElementById('input2');
+const plusBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+const multBtn = document.getElementById('multiplication');
+const divBtn = document.getElementById('division');
+const submitBtn = document.getElementById('submit');
+const resultElement = document.getElementById('result');
+let action = '+';
+
+plusBtn.onclick = () => {
+    action = '+';
+};
+
+minusBtn.onclick = () => {
+    action = '-';
+};
+
+multBtn.onclick = () => {
+    action = '*';
+};
+
+divBtn.onclick = () => {
+    action = '/';
+};
+
+submitBtn.onclick = () => {
+    if (action == '+') {
+        const sum = Number(inputFirst.value) + Number(inputSecond.value);
+        resultElement.textContent = sum;
+    } else  if (action == '-') {
+        const sum = Number(inputFirst.value) - Number(inputSecond.value);
+        resultElement.textContent = sum;
+    } else if (action == '*') {
+        const sum = Number(inputFirst.value) * Number(inputSecond.value);
+        resultElement.textContent = sum;
+    } else if (action == '/') {
+        const sum = Number(inputFirst.value) / Number(inputSecond.value);
+        resultElement.textContent = sum;
+    }
+};
+
+
+
+
+
 

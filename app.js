707b1e98ed19a648789600e7532f8fb5256a7983
pshenichque <pshenichque@mail.ru>
2023-07-24 02:18:34 +0300

    Replace if construction with switch

--- app.js
+++ app.js
@@ -8,20 +8,33 @@ const submitBtn = document.getElementById('submit');
 const resultElement = document.getElementById('result');
 let action = '+';
 
-plusBtn.onclick = () => {
-    action = '+';
-};
+// plusBtn.onclick = () => {
+//     action = '+';
+// };
 
-minusBtn.onclick = () => {
-    action = '-';
-};
+// minusBtn.onclick = () => {
+//     action = '-';
+// };
 
-multBtn.onclick = () => {
-    action = '*';
-};
+// multBtn.onclick = () => {
+//     action = '*';
+// };
 
-divBtn.onclick = () => {
-    action = '/';
+// divBtn.onclick = () => {
+//     action = '/';
+// };
+
+// Делегирование событий https://learn.javascript.ru/switch
+document.getElementById('js-actions').onclick = (event) => {
+    let sign = event.target.dataset.action;
+    if (!sign) { return }
+    switch(sign) {
+        case 'plus': action = '+'; break;
+        case 'minus': action = '-'; break;
+        case 'mult': action = '*'; break;
+        case 'div': action = '/'; break;
+        case 'equal': onEqual(); break;
+    }
 };
 
 function printResult(result) {
@@ -33,27 +46,42 @@ function printResult(result) {
     resultElement.textContent = result;
 };
 
+// Делегирование событий https://learn.javascript.ru/switch
 function computeNumbersWithAction (valFirst, valSecond, actionSymbol) {
     const numberFirst = Number(valFirst.value);
     const numberSecond = Number(valSecond.value);
-    if (actionSymbol == '+') {
-        return numberFirst + numberSecond;
-    }
-    if (actionSymbol == '-') {
-        return numberFirst - numberSecond;
-    }
-    if (actionSymbol == '*') {
-        return numberFirst * numberSecond;
-    }
-    if (actionSymbol == '/') {
-        return numberFirst / numberSecond;
+    // if (actionSymbol === '+') {
+    //     return numberFirst + numberSecond;
+    // }
+    // if (actionSymbol ==='-') {
+    //     return numberFirst - numberSecond;
+    // }
+    // if (actionSymbol === '*') {
+    //     return numberFirst * numberSecond;
+    // }
+    // if (actionSymbol ==='/') {
+    //     return numberFirst / numberSecond;
+    // }
+    switch(actionSymbol) {
+        case '+': return numberFirst + numberSecond;
+        case '-': return numberFirst - numberSecond;
+        case '*': return numberFirst * numberSecond;
+        case '/': return numberFirst / numberSecond;
     }
 };
 
-submitBtn.onclick = () => {
+const onEqual = () => {
     const result = computeNumbersWithAction(inputFirst, inputSecond, action);
     printResult(result);
 };
+
+
+// Свитч вообще имба пиздец запомнить! https://seanbarry.dev/posts/switch-true-pattern
+// switch(true) {
+//     case action === '+' || action === '-': 
+// }
+
+
 
 
 
